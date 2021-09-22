dd9de9d9a00388ffd62624e68f7d9d6317080eae
magzupao <mguado@gmail.com>
2021-09-22 19:01:53 +0200

    Update setup0.sh

--- setup0.sh
+++ setup0.sh
@@ -69,6 +69,11 @@ su - ${USER}
 
 #reiniciamos
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+
 
 
 
