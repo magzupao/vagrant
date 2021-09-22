f66b7c3c810646846b36d2d21dfee2a7e849de3e
magzupao <mguado@gmail.com>
2021-09-22 18:51:30 +0200

    Update setup0.sh

--- setup0.sh
+++ setup0.sh
@@ -36,3 +36,39 @@ wget -c https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.de
 sudo apt-get -y install libappindicator1
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get -y install -f
+
+################################################################################
+# Install  docker
+################################################################################
+echo ' 4. ***************************** '
+sudo apt install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg-agent \
+     software-properties-common
+     
+ curl -fsSL \
+  https://download.docker.com/linux/debian/gpg \
+  | sudo apt-key add -
+ 
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+   
+sudo apt -y update
+
+sudo apt install docker-ce docker-ce-cli containerd.io
+
+apt-cache madison docker-ce
+
+sudo usermod -aG docker ${USER}
+
+su - ${USER}
+
+#reiniciamos
+
+
+
+
